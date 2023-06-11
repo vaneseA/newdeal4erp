dc2f5d0af1973db7433d3cf57cbf1f457647b8ee
DvDK <spol@naver.com>
2023-06-11 21:37:58 +0900

    일매출 작업중.

---
+++ src/main/java/kr/happyjob/study/selSad/controller/SelSadController.java
@@ -0,0 +1,167 @@
+package kr.happyjob.study.selSad.controller;
+
+import kr.happyjob.study.selSad.model.SelSadModel;
+import kr.happyjob.study.selSad.service.SelSadService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/selSad/")
+public class SelSadController {
+
+   @Autowired
+   SelSadService selSadService;
+
+   // Set logger
+   private final Logger logger = LogManager.getLogger(this.getClass());
+
+   // Get class name for logger
+   private final String className = this.getClass().toString();
+
+
+
+   /**
+    * 초기화면
+    */
+   @RequestMapping("notice.do")
+   public String notice(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+                        HttpServletResponse response, HttpSession session) throws Exception {
+
+      logger.info("+ Start " + className + ".notice");
+      logger.info("   - paramMap : " + paramMap);
+
+      logger.info("+ End " + className + ".notice");
+
+      return "mngNot/noticelist";
+   }
+
+   @RequestMapping("saleDay.do")
+   public String noticelist(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+                            HttpServletResponse response, HttpSession session) throws Exception {
+
+      logger.info("+ Start " + className + ".noticelist");
+      logger.info("   - paramMap : " + paramMap);
+
+      int pagenum = Integer.parseInt((String) paramMap.get("pagenum"));
+      int pageSize = Integer.parseInt((String) paramMap.get("pageSize"));
+      int pageindex = (pagenum - 1) * pageSize;
+
+      paramMap.put("pageSize", pageSize);
+      paramMap.put("pageindex", pageindex);
+
+      // Controller  Service  Dao  SQL
+      List<SelSadModel> noticesearchlist = selSadService.noticelist(paramMap);
+      int totalcnt = selSadService.countnoticelist(paramMap);
+
+      model.addAttribute("noticesearchlist", noticesearchlist);
+      model.addAttribute("totalcnt", totalcnt);
+
+
+      logger.info("+ End " + className + ".noticelist");
+
+      return "mngNot/noticelistgrd";
+   }
+
+   @RequestMapping("noticeselectone.do")
+   @ResponseBody
+   public Map<String, Object> noticeselectone(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+                                              HttpServletResponse response, HttpSession session) throws Exception {
+
+      logger.info("+ Start " + className + ".noticeselectone");
+      logger.info("   - paramMap : " + paramMap);
+
+      // Controller  Service  Dao  SQL
+      SelSadModel noticesearch = selSadService.noticeselectone(paramMap);
+
+      Map<String, Object> returnmap = new HashMap<String, Object>();
+
+      returnmap.put("noticesearch", noticesearch);
+
+      logger.info("+ End " + className + ".noticeselectone");
+
+      return returnmap;
+   }
+
+   @RequestMapping("noticesave.do")
+   @ResponseBody
+   public Map<String, Object> noticesave(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+                                         HttpServletResponse response, HttpSession session) throws Exception {
+
+      logger.info("+ Start " + className + ".noticesave");
+      logger.info("   - paramMap : " + paramMap);
+
+      String action = (String) paramMap.get("action");
+
+      paramMap.put("loginid", (String) session.getAttribute("loginId"));
+
+
+      int returncval = 0;
+
+      if("I".equals(action)) {
+         returncval = selSadService.noticeinsert(paramMap);
+      } else if("U".equals(action)) {
+         returncval = selSadService.noticeupdate(paramMap);
+      } else if("D".equals(action)) {
+         returncval = selSadService.noticedelete(paramMap);
+      }
+
+      Map<String, Object> returnmap = new HashMap<String, Object>();
+
+      returnmap.put("returncval", returncval);
+
+      logger.info("+ End " + className + ".noticesave");
+
+      return returnmap;
+   }
+
+
+   @RequestMapping("noticesavefile.do")
+   @ResponseBody
+   public Map<String, Object> noticesavefile(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+                                             HttpServletResponse response, HttpSession session) throws Exception {
+
+      logger.info("+ Start " + className + ".noticesavefile");
+      logger.info("   - paramMap : " + paramMap);
+
+      String action = (String) paramMap.get("action");
+
+      paramMap.put("loginid", (String) session.getAttribute("loginId"));
+
+      int returncval = 0;
+
+      if("I".equals(action)) {
+         returncval = selSadService.noticeinsertfile(paramMap,request);
+      } else if("U".equals(action)) {
+         returncval = selSadService.noticeupdatefile(paramMap,request);
+      } else if("D".equals(action)) {
+         returncval = selSadService.noticedeletefile(paramMap);
+      }
+
+      Map<String, Object> returnmap = new HashMap<String, Object>();
+
+      returnmap.put("returncval", returncval);
+
+      logger.info("+ End " + className + ".noticesavefile");
+
+      return returnmap;
+   }
+
+
+
+
+
+
+}
---
+++ src/main/java/kr/happyjob/study/selSad/service/SelSadService.java
@@ -0,0 +1,40 @@
+package kr.happyjob.study.selSad.service;
+
+import kr.happyjob.study.selSad.model.SelSadModel;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+public interface SelSadService {
+
+	/**  목록 조회 */
+	public List<SelSadModel> noticelist(Map<String, Object> paramMap) throws Exception;
+
+	/** 목록 카운트 조회 */
+	public int countnoticelist(Map<String, Object> paramMap) throws Exception;
+
+	/** 한건 조회 */
+	public SelSadModel noticeselectone(Map<String, Object> paramMap) throws Exception;
+
+	/** 등록 */
+	public int noticeinsert(Map<String, Object> paramMap) throws Exception;
+
+	/** 수정 */
+	public int noticeupdate(Map<String, Object> paramMap) throws Exception;
+
+	/** 삭제 */
+	public int noticedelete(Map<String, Object> paramMap) throws Exception;
+
+	/** 등록 파일 */
+	public int noticeinsertfile(Map<String, Object> paramMap, HttpServletRequest request) throws Exception;
+
+	/** 수정 파일 */
+	public int noticeupdatefile(Map<String, Object> paramMap, HttpServletRequest request) throws Exception;
+
+	/** 삭제  파일*/
+	public int noticedeletefile(Map<String, Object> paramMap) throws Exception;
+
+
+
+}
