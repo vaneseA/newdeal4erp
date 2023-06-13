d1f974c66a6462a2d8f2ebf6faa71dbf0bb77358
DvDK <spol@naver.com>
2023-06-13 09:17:41 +0900

    제품정보부터 되는지 테스트중1//오류
    일,월,연 매출 create 작업 완료//404 오류 수정중
    //폴더명 selSaD이면 500에러가 뜨고 selSad이면 오류가 안남...왜그럴까.
    //그리고 메뉴ID에서도 selSad를 selSaD(대문자)로 바꿨더니 404뜸..
    
    //could not autowire. No beans of ‘LoginDao’ type found
    @어노테이션 추가하면 사라짐

---
+++ src/main/java/busPd/service/BusPdService.java
@@ -0,0 +1,40 @@
+package busPd.service;
+
+import busPd.model.BusPdModel;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+public interface BusPdService {
+
+	/**  목록 조회 */
+	public List<BusPdModel> noticelist(Map<String, Object> paramMap) throws Exception;
+
+	/** 목록 카운트 조회 */
+	public int countnoticelist(Map<String, Object> paramMap) throws Exception;
+
+	/** 한건 조회 */
+	public BusPdModel noticeselectone(Map<String, Object> paramMap) throws Exception;
+
+	/** 등록 */
+	public int noticeinsert(Map<String, Object> paramMap) throws Exception;
+
+	/** 수정 */
+	public int noticeupdate(Map<String, Object> paramMap) throws Exception;
+
+	/** 삭제 */
+	public int noticedelete(Map<String, Object> paramMap) throws Exception;
+
+	/** 등록 파일 */
+	public int noticeinsertfile(Map<String, Object> paramMap, HttpServletRequest request) throws Exception;
+
+	/** 수정 파일 */
+	public int noticeupdatefile(Map<String, Object> paramMap, HttpServletRequest request) throws Exception;
+
+	/** 삭제  파일*/
+	public int noticedeletefile(Map<String, Object> paramMap) throws Exception;
+
+
+
+}
